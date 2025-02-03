664fd141453d413f41c4a0b32cbf83cd50d61b3c
adithya1107 <adigaadithya70@gmail.com>
2025-02-03 11:16:40 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: 3000,       // Ensure a specific port is used
+    strictPort: true, // Ensures the exact port is used
+  }
 })
